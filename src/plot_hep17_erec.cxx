0d05f51a62b3ebadb8651278c9b02a58407165a6
jaehyeok <jaehyeokyoo@gmail.com>
2018-01-09 18:23:32 -0800

    add energy sum over depths vs energy fraction per depth. add plots comparing low and high PU

--- src/plot_hep17_erec.cxx
+++ src/plot_hep17_erec.cxx
@@ -16,14 +16,13 @@
 
 using namespace std;
 
-int rebin=1;
+int rebin=3;
 
 TH1D* renormTH1D(TH1D *h1) 
 { 
   TH1D *h1renorm = static_cast<TH1D*>(h1->Clone());
   for(int ibin=1; ibin<=h1->GetXaxis()->GetNbins(); ibin++) 
   {
-    cout << ibin << " " << h1->GetXaxis()->GetBinUpEdge(ibin) <<  " " << h1->GetXaxis()->GetBinLowEdge(ibin) << " " << h1->GetBinContent(ibin) << endl;
     h1renorm->SetBinContent(ibin, h1->GetBinContent(ibin)/(h1->GetXaxis()->GetBinUpEdge(ibin)-h1->GetXaxis()->GetBinLowEdge(ibin)));
   }
   return h1renorm;
@@ -32,10 +31,12 @@ TH1D* renormTH1D(TH1D *h1)
 int main()
 {
   TFile* infile = TFile::Open("hep17_erec.root"); 
-
+  
+  //
+  // Compare different datasets
+  // 
   for(int invtx=0; invtx<3; invtx++) 
   { 
-    if(invtx!=0) continue; // FIXME 
 
     TH1D *h1_zero[14][4][7]; // ieta iphi depth 
     TH1D *h1_jetht[14][4][7]; // ieta iphi depth 
@@ -61,13 +62,13 @@ int main()
             h1_zero[ieta-16][iphi-63][depth-1]->Rebin(rebin);
             h1cosmetic(h1_zero[ieta-16][iphi-63][depth-1],  Form("ieta=%i,iphi=%i,depth=%i%s", ieta,iphi,depth,nvtxcut.Data()), kBlack, 1, 0, "E_{rechit} [GeV]"); 
             h1_zero[ieta-16][iphi-63][depth-1]->SetMarkerSize(0); 
-            h1_zero[ieta-16][iphi-63][depth-1]->GetXaxis()->SetRange(1,10); 
+            h1_zero[ieta-16][iphi-63][depth-1]->GetXaxis()->SetRange(2,10000); 
           }
           else 
           { 
             h1_zero[ieta-16][iphi-63][depth-1] = new TH1D(Form("h1_zero_erec_ieta%i_iphi%i_depth%i_nvtx%i", ieta,iphi,depth,invtx),Form("h1_zero_erec_ieta%i_iphi%i_depth%i_nvtx%i", ieta,iphi,depth,invtx),1,0,1);
           } 
-
+         
           if(infile->Get(Form("h1_jetht_erec_ieta%i_iphi%i_depth%i_nvtx%i", ieta,iphi,depth,invtx)))
           {
             h1_jetht[ieta-16][iphi-63][depth-1]
@@ -75,7 +76,7 @@ int main()
             h1_jetht[ieta-16][iphi-63][depth-1]->Rebin(rebin);
             h1cosmetic(h1_jetht[ieta-16][iphi-63][depth-1], Form("ieta=%i,iphi=%i,depth=%i%s", ieta,iphi,depth,nvtxcut.Data()), kRed,   1, 0, "E_{rechit} [GeV]"); 
             h1_jetht[ieta-16][iphi-63][depth-1]->SetMarkerSize(0);
-            h1_jetht[ieta-16][iphi-63][depth-1]->GetXaxis()->SetRange(1,10); 
+            h1_jetht[ieta-16][iphi-63][depth-1]->GetXaxis()->SetRange(2,10000); 
           }
           else 
           { 
@@ -89,7 +90,7 @@ int main()
             h1_met[ieta-16][iphi-63][depth-1]->Rebin(rebin);
             h1cosmetic(h1_met[ieta-16][iphi-63][depth-1], Form("ieta=%i,iphi=%i,depth=%i%s", ieta,iphi,depth,nvtxcut.Data()), kGreen+1,   1, 0, "E_{rechit} [GeV]"); 
             h1_met[ieta-16][iphi-63][depth-1]->SetMarkerSize(0);
-            h1_met[ieta-16][iphi-63][depth-1]->GetXaxis()->SetRange(1,10); 
+            h1_met[ieta-16][iphi-63][depth-1]->GetXaxis()->SetRange(2,10000); 
           }
           else 
           { 
@@ -103,7 +104,7 @@ int main()
             h1_pion[ieta-16][iphi-63][depth-1]->Rebin(rebin);
             h1cosmetic(h1_pion[ieta-16][iphi-63][depth-1], Form("ieta=%i,iphi=%i,depth=%i%s", ieta,iphi,depth,nvtxcut.Data()), kBlue,   1, 0, "E_{rechit} [GeV]"); 
             h1_pion[ieta-16][iphi-63][depth-1]->SetMarkerSize(0);
-            h1_pion[ieta-16][iphi-63][depth-1]->GetXaxis()->SetRange(1,10); 
+            h1_pion[ieta-16][iphi-63][depth-1]->GetXaxis()->SetRange(2,10000); 
           }
           else 
           { 
@@ -207,5 +208,117 @@ int main()
       //delete h1_pion_ratio;
       delete c;
     }
+  }
+  
+  //
+  // compare high and low PU 
+  //
+  TH1D *h1_zero_nvtx1[14][4][7];  
+  TH1D *h1_zero_nvtx2[14][4][7];  
+
+  for(int ieta=16; ieta<30; ieta++)
+  {
+    for(int iphi=63; iphi<67; iphi++)
+    {
+      for(int depth=1; depth<8; depth++)
+      {
+        int invtx=1;
+        if(infile->Get(Form("h1_zero_erec_ieta%i_iphi%i_depth%i_nvtx%i", ieta,iphi,depth,invtx))) 
+        { 
+          cout << "ZB: " << Form("ieta %i, iphi%i, depth%i, nvtx%i", ieta,iphi,depth,invtx) << endl;
+          h1_zero_nvtx1[ieta-16][iphi-63][depth-1]
+            = renormTH1D(static_cast<TH1D*>(infile->Get(Form("h1_zero_erec_ieta%i_iphi%i_depth%i_nvtx%i", ieta,iphi,depth,invtx))));
+          h1_zero_nvtx1[ieta-16][iphi-63][depth-1]->Rebin(rebin);
+          h1cosmetic(h1_zero_nvtx1[ieta-16][iphi-63][depth-1],  Form("ieta=%i,iphi=%i,depth=%i", ieta,iphi,depth), kBlue, 1, 0, "E_{rechit} [GeV]"); 
+          h1_zero_nvtx1[ieta-16][iphi-63][depth-1]->SetMarkerSize(0); 
+          h1_zero_nvtx1[ieta-16][iphi-63][depth-1]->GetXaxis()->SetRange(2,10000); 
+        }
+        else 
+        { 
+          h1_zero_nvtx1[ieta-16][iphi-63][depth-1] = new TH1D(Form("h1_zero_erec_ieta%i_iphi%i_depth%i_nvtx%i", ieta,iphi,depth,invtx),Form("h1_zero_erec_ieta%i_iphi%i_depth%i_nvtx%i", ieta,iphi,depth,invtx),1,0,1);
+        } 
+        invtx=2;
+        if(infile->Get(Form("h1_zero_erec_ieta%i_iphi%i_depth%i_nvtx%i", ieta,iphi,depth,invtx))) 
+        { 
+          cout << "ZB: " << Form("ieta %i, iphi%i, depth%i, nvtx%i", ieta,iphi,depth,invtx) << endl;
+          h1_zero_nvtx2[ieta-16][iphi-63][depth-1]
+            = renormTH1D(static_cast<TH1D*>(infile->Get(Form("h1_zero_erec_ieta%i_iphi%i_depth%i_nvtx%i", ieta,iphi,depth,invtx))));
+          h1_zero_nvtx2[ieta-16][iphi-63][depth-1]->Rebin(rebin);
+          h1cosmetic(h1_zero_nvtx2[ieta-16][iphi-63][depth-1],  Form("ieta=%i,iphi=%i,depth=%i", ieta,iphi,depth), kRed, 1, 0, "E_{rechit} [GeV]"); 
+          h1_zero_nvtx2[ieta-16][iphi-63][depth-1]->SetMarkerSize(0); 
+          h1_zero_nvtx2[ieta-16][iphi-63][depth-1]->GetXaxis()->SetRange(2,10000); 
+        }
+        else 
+        { 
+          h1_zero_nvtx2[ieta-16][iphi-63][depth-1] = new TH1D(Form("h1_zero_erec_ieta%i_iphi%i_depth%i_nvtx%i", ieta,iphi,depth,invtx),Form("h1_zero_erec_ieta%i_iphi%i_depth%i_nvtx%i", ieta,iphi,depth,invtx),1,0,1);
+        } 
+      }
+    }
+  }
+
+  TLegend *l1 = new TLegend(0.6, 0.7, 0.90, 0.90);
+  l1->SetNColumns(1);
+  l1->SetBorderSize(0);
+  l1->SetFillColor(0);
+  l1->SetFillStyle(0);
+  l1->SetTextAlign(12);
+  //l1->SetTextSize(0.06);
+  l1->SetFillColor(kWhite);
+  l1->SetLineColor(kWhite);
+  l1->SetShadowColor(kWhite);
+  l1->AddEntry(h1_zero_nvtx1[5][0][0],   " ZeroBias, Nvtx<25",  "l");
+  l1->AddEntry(h1_zero_nvtx2[5][0][0],   " ZeroBias, Nvtx>45",  "l");
+
+  //
+  for(int ieta=16; ieta<30; ieta++)
+  {
+    TH1D *h1_zero_ratio[4][7]; 
+    TCanvas *c = new TCanvas("c","c",3200,1800);
+    c->SetGrid(1,1); 
+    c->Divide(7,4); 
+    for(int iphi=63; iphi<67; iphi++)
+    {
+      for(int depth=1; depth<8; depth++) 
+      {
+        if(h1_zero_nvtx1[ieta-16][iphi-63][depth-1]->Integral()!=0) 
+        { 
+          h1_zero_ratio[iphi-63][depth-1] = static_cast<TH1D*>(h1_zero_nvtx2[ieta-16][iphi-63][depth-1]->Clone()); 
+          h1_zero_ratio[iphi-63][depth-1]->Divide(h1_zero_nvtx1[ieta-16][iphi-63][depth-1]);
+          c->cd((iphi-63)*7+depth);
+          // pad 1
+          TPad *pad1 = new TPad("p_main", "p_main", 0.0, 0.25, 1.0, 1.0);
+          pad1->Draw();
+          pad1->cd();
+          pad1->cd()->SetLogx(1);
+          pad1->cd()->SetLogy(1); 
+          h1_zero_nvtx2[ieta-16][iphi-63][depth-1]->GetXaxis()->SetLabelSize(0.0);
+          h1_zero_nvtx2[ieta-16][iphi-63][depth-1]->GetYaxis()->SetLabelSize(0.06);
+          h1_zero_nvtx2[ieta-16][iphi-63][depth-1]->Draw("e");
+          h1_zero_nvtx1[ieta-16][iphi-63][depth-1]->Draw("e same");
+          l1->Draw("same");
+          // pad 2 
+          c->cd((iphi-63)*7+depth);
+          TPad *pad2 = new TPad("p_pull", "p_pull", 0.0, 0.0, 1.0, 0.3);
+          pad2->SetBottomMargin(0.4); 
+          pad2->Draw();
+          pad2->cd();
+          pad2->cd()->SetLogx(1);
+          h1_zero_ratio[iphi-63][depth-1]->SetTitle("");
+          h1_zero_ratio[iphi-63][depth-1]->SetMaximum(4);
+          h1_zero_ratio[iphi-63][depth-1]->SetMinimum(0); 
+          h1_zero_ratio[iphi-63][depth-1]->GetYaxis()->SetNdivisions(505);;
+          h1_zero_ratio[iphi-63][depth-1]->GetYaxis()->SetTitle("Ratio");
+          h1_zero_ratio[iphi-63][depth-1]->GetYaxis()->SetTitleSize(0.15);
+          h1_zero_ratio[iphi-63][depth-1]->GetYaxis()->SetLabelSize(0.15);
+          h1_zero_ratio[iphi-63][depth-1]->GetXaxis()->SetLabelSize(0.15);
+          h1_zero_ratio[iphi-63][depth-1]->GetXaxis()->SetTitle("E_{rec} [GeV]");
+          h1_zero_ratio[iphi-63][depth-1]->GetXaxis()->SetTitleSize(0.15);
+          h1_zero_ratio[iphi-63][depth-1]->GetXaxis()->SetTitleOffset(1.2);
+          h1_zero_ratio[iphi-63][depth-1]->Draw("e");
+        }
+      }
+    }
+    c->Print(Form("plots/h1_zero_erec_ieta%i.pdf", ieta));
+    delete c;
   }
 }
