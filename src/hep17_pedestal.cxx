362c2c5719a131bf56d536b385171dce6233f293
Jae Hyeok Yoo <jaehyeokyoo@gmail.com>
2018-01-18 22:21:31 -0800

    update with raddam correction

--- src/hep17_pedestal.cxx
+++ src/hep17_pedestal.cxx
@@ -17,13 +17,16 @@
 using namespace std; 
 
 bool do8ts=false; 
+bool doRaddam=true; 
 
 //int main(int argc, char *argv[])
 int main()
 {
+  float raddam[13]={0.978,0.976,0.986,0.977,0.944,0.954,0.941,0.923,0.912,0.879,0.855,0.830,0.817};
+  
   gErrorIgnoreLevel=kError+1;
 
-  hcal_tree_noise ped("/Users/jaehyeok/Downloads/PFGtuples/SPE_gsel0_307580.root");
+  hcal_tree_noise ped("/homes/jaehyeokyoo/scratch/SPE_gsel0_306233.root");
 
   // Need Rrec for 
   //  - each channel 
@@ -76,10 +79,17 @@ int main()
       //DEBUG cout << ped.QIE11DigiIEta().at(idigi) << " " << ped.QIE11DigiIPhi().at(idigi) << " " << ped.QIE11DigiDepth().at(idigi) << " : "  << ped.QIE11DigiFC().at(idigi).size() << endl;
     } 
   }
-
-
+ 
+  //
+  TH1D *h1_frac = new TH1D("h1_frac","h1_frac",20,0,1);
+  
+  //
+  // pedestal distributions 
+  //
   for(int ieta=16; ieta<30; ieta++)
-  {
+  { 
+    int cut=76;
+    if(ieta>16 && doRaddam) cut = 76*raddam[ieta-17];
     TCanvas *c = new TCanvas("c","c",3200,1800);
     c->SetGrid(1,1); 
     c->Divide(7,4); 
@@ -96,13 +106,27 @@ int main()
         h1[ieta-16][iphi-63][depth-1]->SetTitle(Form("(%i,%i,%i), %.0f#pm%.0f, %.4f %%", ieta,iphi,depth,
                             h1[ieta-16][iphi-63][depth-1]->GetMean(),
                             h1[ieta-16][iphi-63][depth-1]->GetRMS(),
-                            h1[ieta-16][iphi-63][depth-1]->Integral(depth==1?51:101,1000)/h1[ieta-16][iphi-63][depth-1]->Integral()*100));
+                            h1[ieta-16][iphi-63][depth-1]->Integral(cut,1000)/h1[ieta-16][iphi-63][depth-1]->Integral()*100));
         h1[ieta-16][iphi-63][depth-1]->Draw("hist"); 
-        cout << Form("(%i,%i,%i) %.4f %%", ieta,iphi,depth,h1[ieta-16][iphi-63][depth-1]->Integral(depth==1?101:101,1000)/h1[ieta-16][iphi-63][depth-1]->Integral()*100) << endl;
+        cout << Form("(%i,%i,%i) %.4f %%", ieta,iphi,depth,h1[ieta-16][iphi-63][depth-1]->Integral(cut,1000)/h1[ieta-16][iphi-63][depth-1]->Integral()*100) << endl;
+        FillTH1D(h1_frac,h1[ieta-16][iphi-63][depth-1]->Integral(cut,1000)/h1[ieta-16][iphi-63][depth-1]->Integral()*100,1.);
       }
     }
     c->Print(Form("plots/h1_ped_ieta%i_%s.pdf", ieta, do8ts?"8ts":"2ts"));
     delete c;
-  }
+  } 
+
+  // 
+  // Summary of fractions 
+  //  
+  TCanvas *c_frac = new TCanvas("c_frac","c_frac",400,400);
+  c_frac->cd(1);
+  h1_frac->SetLineColor(kBlack);
+  h1_frac->SetLineWidth(2);
+  h1_frac->SetFillColor(kAzure-9);
+  h1_frac->Draw("hist");
+  c_frac->Print(Form("plots/h1_frac_%s.pdf",doRaddam?"Raddam":"noRaddam"));
+
 }
+
 
