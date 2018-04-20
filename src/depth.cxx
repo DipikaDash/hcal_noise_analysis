0f579fa86b276a490ae53b57b8437027a43edb4a
jaehyeok <jaehyeokyoo@gmail.com>
2018-04-19 18:02:51 -0700

    add more plots

--- src/depth.cxx
+++ src/depth.cxx
@@ -56,7 +56,10 @@ int main()
   TH2D *h2_dRvsEta  = new TH2D("h2_dRvsEta", "h2_dRvsEta", 100, 0., 10., 100, -3.5, 3.5);
   TH2D *h2_dEtavsdPhi  = new TH2D("h2_dEtavsdPhi", "h2_dEtavsdPhi", 100, -10., 10., 50, -3.14, 3.14);
   TH1D *h1_dPhi  = new TH1D("h1_dPhi", "h1_dPhi", 30, 0., 3.);
-  
+  TH2D *h2_JetPtvsSumE  = new TH2D("h2_JetPtvsSumE", "h2_JetPtvsSumE", 100, 0., 1000., 100, 0, 1000);
+  TH1D *h1_ratio  = new TH1D("h1_ratio", "h1_ratio", 50, -2., 2.);
+ 
+  // DEBUG 
   TH1D *h1_dR_  = new TH1D("h1_dR_", "h1_dR_", 100, 0., 10.);
   TH2D *h2_dRvsPhi_  = new TH2D("h2_dRvsPhi_", "h2_dRvsPhi_", 100, 0., 10., 100, -3.14, 3.14);
   TH2D *h2_dRvsEta_  = new TH2D("h2_dRvsEta_", "h2_dRvsEta_", 100, 0., 10., 100, -3.5, 3.5);
@@ -69,15 +72,16 @@ int main()
 
     for(unsigned int ijet=0; ijet<tree_nu.JetPt().size(); ijet++)
     { 
+      float esum_rh = 0;
       if(Abs(tree_nu.JetEta().at(ijet))<2 || Abs(tree_nu.JetEta().at(ijet))>2.5) continue; // Select jets well within HE
-      if(tree_nu.JetPt().at(ijet)<300) continue;
+      if(tree_nu.JetPt().at(ijet)<30) continue;
       for(unsigned int irec=0; irec<tree_nu.HBHERecHitEnergy().size(); irec++)
       { 
         bool isHE = true;
         if(Abs(tree_nu.HBHERecHitIEta().at(irec))<16) isHE = false;
         if(Abs(tree_nu.HBHERecHitIEta().at(irec))==16 && tree_nu.HBHERecHitDepth().at(irec)!=4) isHE = false;
         if(!isHE) continue;
-        if(tree_nu.HBHERecHitEnergy().at(irec)<5) continue;
+        //if(tree_nu.HBHERecHitEnergy().at(irec)<5) continue;
         
         float deltaR = dR(tree_nu.JetEta().at(ijet), tree_nu.JetPhi().at(ijet),
                           tree_nu.HBHERecHitEta().at(irec), tree_nu.HBHERecHitPhi().at(irec));
@@ -96,15 +100,21 @@ int main()
              << tree_nu.HBHERecHitPhi().at(irec) << " " 
              << tree_nu.HBHERecHitDepth().at(irec) << " " 
              << tree_nu.HBHERecHitEnergy().at(irec) <<  " = " 
-             << deltaR << endl;
-      //  if(deltaR>0.3) continue;
+             << deltaR << endl; 
+
+        if(deltaR>0.5) continue;
+        float theta = 2*TMath::ATan(TMath::Exp(-1*tree_nu.HBHERecHitEta().at(irec)));  
+        // theta is w.r.t. z axis
+        esum_rh = esum_rh +  TMath::Sin(theta)*tree_nu.HBHERecHitEnergy().at(irec);
       }
-       
+      FillTH2D(h2_JetPtvsSumE, tree_nu.JetPt().at(ijet), esum_rh, 1); 
+      FillTH1D(h1_ratio, esum_rh/tree_nu.JetPt().at(ijet), 1); 
+
       // DEBUGGING 
       for(unsigned int ijet2=0; ijet2<tree_nu.JetPt().size(); ijet2++) 
       { 
         if(ijet==ijet2) continue;
-        if(tree_nu.JetPt().at(ijet2)<100) continue;
+        if(tree_nu.JetPt().at(ijet2)<30) continue;
         float deltaR = dR(tree_nu.JetEta().at(ijet), tree_nu.JetPhi().at(ijet),
             tree_nu.JetEta().at(ijet2), tree_nu.JetPhi().at(ijet2));
         FillTH1D(h1_dR_, deltaR, 1); 
@@ -127,6 +137,8 @@ int main()
   c->cd(6); c->cd(6)->SetLogz(1); h2_dRvsEta_->Draw("colz");
   c->cd(7); c->cd(7)->SetLogz(1); h2_dEtavsdPhi->Draw("colz");
   c->cd(8); c->cd(8)->SetLogy(1); h1_dPhi->Draw("hist");
+  c->cd(9); c->cd(9)->SetLogz(1); h2_JetPtvsSumE->Draw("colz");
+  c->cd(10); c->cd(10)->SetLogy(10); h1_ratio->Draw("hist");
   c->Print("plots/depth.pdf");
   
 
